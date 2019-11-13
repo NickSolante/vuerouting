9a36ddec0a60722cd3e4cf9654879ae9c2e37aa3
frostnovus <psolante@deakin.edu.au>
2019-11-13 18:46:50 +1100

    semantic ui is in the app

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <AppHeader></AppHeader>
+
+</div>
+  
+</template>
+<script>
+import AppHeader from './components/AppHeader'
+export default {
+  components: {
+    AppHeader
+  }
+}
+</script>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="ui secondary pointing menu">
+  <a href="/" class="active item">
+    Image Storage
+  </a>
+  <a class="item">
+    Messages
+  </a>
+  <a class="item">
+    Friends
+  </a>
+  <div class="right menu">
+    <a class="ui item">
+      Logout
+    </a>
+  </div>
+</div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
