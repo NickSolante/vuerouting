154472341350e3c09f8d66c4f5b40b4adbce6000
frostnovus <psolante@deakin.edu.au>
2019-11-21 21:10:35 +1100

    puah

---
+++ src/store/modules/auth.js
@@ -0,0 +1,22 @@
+const state = {
+  token: null
+}
+
+const getters = {
+  isLoggedIn: state => !!state.token
+}
+
+const actions = {
+  logout: ({ commit }) => {
+    commit('setToken', null)
+  }
+}
+
+const mutations = {
+  setToken: (state, token) => {
+    state.token = token
+  }
+}
+//api.imgur.com/oauth2/authorize?
+https: client_id = YOUR_CLIENT_ID & response_type = REQUESTED_RESPONSE_TYPE &
+state = APPLICATION_STATE
